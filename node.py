5c45a949e55aba2c421841940c2b5cc062e8f4b5
PathakPratik <rajpratik10@gmail.com>
2021-11-24 12:27:57 +0000

    added listentosensor method in node

--- node.py
+++ node.py
@@ -10,11 +10,12 @@ MULTICAST_TTL = constants.MULTICAST_TTL
 
 class Node:
     
-    def __init__(self, host, port):
+    def __init__(self, host, port, hostname):
         self.host = host
+        self.hostname = hostname
         self.port = port
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-        print(self.sock)
+        self.unicastsock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 
     def handleClient(self):
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -24,11 +25,6 @@ class Node:
         else:
             # on this port, listen ONLY to MCAST_GRP
             self.sock.bind((MCAST_GRP, MCAST_PORT))
-        
-        print((MCAST_GRP, MCAST_PORT))
-        print((self.host, constants.SENSOR_PORT))
-        # This port will listen to unicast sensor communication
-        self.sock.bind((self.host, constants.SENSOR_PORT))
 
         mreq = struct.pack("4sl", socket.inet_aton(MCAST_GRP), socket.INADDR_ANY)
         self.sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
@@ -42,6 +38,19 @@ class Node:
             #     print("IP address list: ", addr_list)
             #     print(data)
         
+    def listenToSensor(self):
+        self.unicastsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        
+        # This port will listen to unicast sensor communication
+        self.unicastsock.bind((self.hostname, constants.SENSOR_PORT))
+
+        mreq = struct.pack("4sl", socket.inet_aton(MCAST_GRP), socket.INADDR_ANY)
+        self.unicastsock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+
+        while True:
+            data, addr = self.unicastsock.recvfrom(10240)
+            print(data)
+
     #Function to check if there is a drop in connection of any node
     def checkNodes(self):
         pass
@@ -50,6 +59,9 @@ class Node:
         listenerThread = threading.Thread(target=self.handleClient)
         listenerThread.setDaemon(True)
         listenerThread.start()
+        SensorThread = threading.Thread(target=self.listenToSensor)
+        SensorThread.setDaemon(True)
+        SensorThread.start()
         print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")    
         print("[STARTING] server is starting...")
         # checkNodesThread = threading.Thread(target=self.checkNodes)
@@ -66,13 +78,12 @@ class Node:
 def main():
     hostname = socket.gethostname()
     host = socket.gethostbyname(hostname)
-    print(hostname,host)
-    node = Node(host, MCAST_PORT)
+    node = Node(host, MCAST_PORT, hostname)
     node.start()
     # to test mcast handler is working
     node.sock.sendto(b"robot", (MCAST_GRP, MCAST_PORT))
     # to test unicast sensor handler is working
-    node.sock.sendto(b"sensortest", (host, constants.SENSOR_PORT))
+    node.unicastsock.sendto(b"sensortest", (hostname, constants.SENSOR_PORT))
     while True:
         pass
     
