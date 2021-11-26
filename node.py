94b4da1bd2d19c498c194886903cc6e6149c06b0
Catess <bhattaa1@tcd.ie>
2021-11-26 19:09:31 +0000

    Push IP into list

--- node.py
+++ node.py
@@ -2,13 +2,17 @@ import socket
 import struct
 import threading
 import constants
+import time
 
 MCAST_GRP = constants.MCAST_GRP
-MCAST_PORT = constants.MCAST_PORT
+MCAST_DISC_PORT = constants.MCAST_DISC_PORT
 IS_ALL_GROUPS = True
 MULTICAST_TTL = constants.MULTICAST_TTL
 
 class Node:
+    nodeTimestampList = {}
+    addrList = []
+    nodeName = ""
     
     def __init__(self, host, port, hostname):
         self.host = host
@@ -16,28 +20,31 @@ class Node:
         self.port = port
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
         self.unicastsock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+        
+    def setNodeName(self,name):
+        self.nodeName = name
 
-    def handleClient(self):
+    def handleDiscovery(self):
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         if IS_ALL_GROUPS:
             # on this port, receives ALL multicast groups
-            self.sock.bind(('', MCAST_PORT))
+            self.sock.bind(('', MCAST_DISC_PORT))
         else:
             # on this port, listen ONLY to MCAST_GRP
-            self.sock.bind((MCAST_GRP, MCAST_PORT))
+            self.sock.bind((MCAST_GRP, MCAST_DISC_PORT))
 
         mreq = struct.pack("4sl", socket.inet_aton(MCAST_GRP), socket.INADDR_ANY)
         self.sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
         while True:
             data, addr = self.sock.recvfrom(10240)
-            print(data)
-            # print(addr)
-            # if addr not in addr_list:
-            #     addr_list.append(addr)
-            #     print("IP address list: ", addr_list)
-            #     print(data)
-        
+            print("Received message in handleDiscovery: ", data, " from IP: ", addr)
+            if addr not in self.addrList:
+                self.addrList.append(addr)
+                print("handleDiscovery New IP address entry into list: ", self.addrList)
+            self.nodeTimestampList[addr] = time.time()
+            print("handleDiscovery IP Address List: ",self.nodeTimestampList)
+
     def listenToSensor(self):
         # Remove this after we decide how to adress each node
         self.unicastsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -54,20 +61,30 @@ class Node:
         pass
         
     def start(self):
-        listenerThread = threading.Thread(target=self.handleClient)
+        self.setNodeName("root")
+        listenerThread = threading.Thread(target=self.handleDiscovery)
         listenerThread.setDaemon(True)
         listenerThread.start()
         SensorThread = threading.Thread(target=self.listenToSensor)
         SensorThread.setDaemon(True)
-        SensorThread.start()
+        #SensorThread.start()
         print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")    
         print("[STARTING] server is starting...")
         # checkNodesThread = threading.Thread(target=self.checkNodes)
         # checkNodesThread.start()
+        broadcastNodeThread = threading.Thread(target=self.broadcastNode)
+        broadcastNodeThread.start()
+        
+    def broadcastNode(self):
+        while(True):
+            self.sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, MULTICAST_TTL)
+            self.sock.sendto(self.nodeName.encode('utf-8'), (MCAST_GRP, MCAST_DISC_PORT))
+            print(self.nodeName, " has broadcasted...")
+            time.sleep(10)
 
     def sendMsgToAll(strMsg):
         sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, MULTICAST_TTL)
-        sock.sendto(b"strMsg", (MCAST_GRP, MCAST_PORT))
+        sock.sendto(b"strMsg", (MCAST_GRP, MCAST_DISC_PORT))
         
     def sendMsgToNode(strMsg, ip_addr):
         unicastSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
@@ -76,12 +93,12 @@ class Node:
 def main():
     hostname = socket.gethostname()
     host = socket.gethostbyname(hostname)
-    node = Node(host, MCAST_PORT, hostname)
+    node = Node(host, MCAST_DISC_PORT, hostname)
     node.start()
     # to test mcast handler is working
-    node.sock.sendto(b"robot", (MCAST_GRP, MCAST_PORT))
+    node.sock.sendto(b"robot", (MCAST_GRP, MCAST_DISC_PORT))
     # to test unicast sensor handler is working
-    node.unicastsock.sendto(b"sensortest", (hostname, constants.SENSOR_PORT))
+    #node.unicastsock.sendto(b"sensortest", (hostname, constants.SENSOR_PORT))
     while True:
         pass
     
