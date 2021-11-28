19e3b06b11e9af8e84e33a9db13132456caebd20
PathakPratik <rajpratik10@gmail.com>
2021-11-28 12:17:39 +0000

    added ospf algo for routing

--- node.py
+++ node.py
@@ -3,15 +3,20 @@ import struct
 import threading
 import constants
 import time
+import sys
+import json
 
 MCAST_GRP = constants.MCAST_GRP
 MCAST_DISC_PORT = constants.MCAST_DISC_PORT
+MCAST_ROUTING_PORT = constants.MCAST_ROUTING_PORT
 IS_ALL_GROUPS = True
 MULTICAST_TTL = constants.MULTICAST_TTL
 
 class Node:
     nodeTimestampList = {}
     nodeName = ""
+    routingDB = {}
+    discoverDB = {}
     
     def __init__(self, host, port, hostname):
         self.host = host
@@ -19,9 +24,47 @@ class Node:
         self.port = port
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
         self.unicastsock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+        self.routingsock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
         
-    def setNodeName(self,name):
-        self.nodeName = name
+    def setNodeName(self):
+        self.nodeName = str(sys.argv[1])
+        
+    def parseJsonDataToRoutingDB(broadcastDBJson):
+        pass
+        
+    def handleRouting(self):
+        self.routingsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        if IS_ALL_GROUPS:
+            # on this port, receives ALL multicast groups
+            self.routingsock.bind(('', MCAST_ROUTING_PORT))
+        else:
+            # on this port, listen ONLY to MCAST_GRP
+            self.routingsock.bind((MCAST_GRP, MCAST_ROUTING_PORT))
+
+        mreq = struct.pack("4sl", socket.inet_aton(MCAST_GRP), socket.INADDR_ANY)
+        self.routingsock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+
+        while True:
+            data, addr = self.routingsock.recvfrom(10240)
+            broadcastDBJson = data.decode("utf-8")
+            print("BROADCAST DB RECEIVED:::: ", broadcastDBJson)
+            #parseJsonDataToRoutingDB(broadcastDBJson)
+            
+    def broadcastDatabase(self):
+        while(True):
+            #Create the routing database
+            for node in self.discoverDB:
+                values = self.discoverDB[node]
+                self.routingDB[node] = values
+                print("Routing DB new entry::: ", self.routingDB)
+            print("Routing DB: ", self.routingDB)
+            self.sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, MULTICAST_TTL)
+            dict_json = json.dumps(self.routingDB)
+            print("!!!!!!!!!!!!!", dict_json)
+            self.sock.sendto(dict_json.encode('utf-8'), (MCAST_GRP, MCAST_ROUTING_PORT))
+            print(self.nodeName, " has broadcasted its database...")
+            time.sleep(30)
+
 
     def handleDiscovery(self):
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -37,9 +80,19 @@ class Node:
 
         while True:
             data, addr = self.sock.recvfrom(10240)
-            print(time.time(), "::Received message in handleDiscovery: ", data, " from IP: ", addr)
-            self.nodeTimestampList[addr] = time.time()
+            nodeName = data.decode("utf-8")
+            print(time.time(), "::Received message in handleDiscovery: ", nodeName, " from IP: ", addr)
+            self.nodeTimestampList[nodeName] = time.time()
             print("handleDiscovery IP Address List: ",self.nodeTimestampList)
+            if addr not in list(self.nodeTimestampList):
+                self.discoverDB[nodeName] = list()
+                values = [addr, 1]
+                self.discoverDB[nodeName].extend(values)
+                
+            print("Node ", self.nodeName, "connected to: ", self.discoverDB[nodeName])
+            detail = self.discoverDB[nodeName]
+            print(detail[0])
+            print(detail[1])
 
     def listenToSensor(self):
         # Remove this after we decide how to adress each node
@@ -66,30 +119,46 @@ class Node:
                     
             time.sleep(5)
         
-    def start(self):
-        self.setNodeName("root")
-        listenerThread = threading.Thread(target=self.handleDiscovery)
-        listenerThread.setDaemon(True)
-        listenerThread.start()
+    def start(self, nodeName):
+        self.setNodeName()
+        
+        discoveryThread = threading.Thread(target=self.handleDiscovery)
+        discoveryThread.setDaemon(True)
+        discoveryThread.start()
+        
         SensorThread = threading.Thread(target=self.listenToSensor)
         SensorThread.setDaemon(True)
         SensorThread.start()
+        
         print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")    
         print("[STARTING] server is starting...")
+        
         checkNodesThread = threading.Thread(target=self.checkNodes)
         checkNodesThread.setDaemon(True)
         checkNodesThread.start()
+        
         broadcastNodeThread = threading.Thread(target=self.broadcastNode)
         broadcastNodeThread.setDaemon(True)
         broadcastNodeThread.start()
+        
+        time.sleep(15)
+        
+        routingThread = threading.Thread(target=self.handleRouting)
+        routingThread.setDaemon(True)
+        routingThread.start()
+        
+        broadcastDatabaseThread = threading.Thread(target=self.broadcastDatabase)
+        routingThread.setDaemon(True)
+        broadcastDatabaseThread.start()
         
     def broadcastNode(self):
         while(True):
             self.sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, MULTICAST_TTL)
             self.sock.sendto(self.nodeName.encode('utf-8'), (MCAST_GRP, MCAST_DISC_PORT))
             print(self.nodeName, " has broadcasted...")
-            time.sleep(10)
-
+            time.sleep(30)
+            
+    
     def sendMsgToAll(strMsg):
         sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, MULTICAST_TTL)
         sock.sendto(b"strMsg", (MCAST_GRP, MCAST_DISC_PORT))
@@ -102,9 +171,10 @@ def main():
     hostname = socket.gethostname()
     host = socket.gethostbyname(hostname)
     node = Node(host, MCAST_DISC_PORT, hostname)
-    node.start()
+    
+    node.start(str(sys.argv[1]))
     # to test mcast handler is working
-    node.sock.sendto(b"robot", (MCAST_GRP, MCAST_DISC_PORT))
+    #node.sock.sendto(b"robot", (MCAST_GRP, MCAST_DISC_PORT))
     # to test unicast sensor handler is working
     #node.unicastsock.sendto(b"sensortest", (hostname, constants.SENSOR_PORT))
     while True:
