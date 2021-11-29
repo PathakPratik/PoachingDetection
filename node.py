8b37c413358d7cb9ae33ba1558ba9922355ab456
Catess <bhattaa1@tcd.ie>
2021-11-29 18:14:35 +0000

    routing mechanism latest

--- node.py
+++ node.py
@@ -29,9 +29,6 @@ class Node:
     def setNodeName(self):
         self.nodeName = str(sys.argv[1])
         
-    def parseJsonDataToRoutingDB(broadcastDBJson):
-        pass
-        
     def handleRouting(self):
         self.routingsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         if IS_ALL_GROUPS:
@@ -48,7 +45,20 @@ class Node:
             data, addr = self.routingsock.recvfrom(10240)
             broadcastDBJson = data.decode("utf-8")
             print("BROADCAST DB RECEIVED:::: ", broadcastDBJson)
-            #parseJsonDataToRoutingDB(broadcastDBJson)
+            currentValue = self.routingDB[key]
+                newValue = db[key]
+                newValue[1] = newValue[1]+1   #Increment hop
+                
+                if key == self.nodeName:
+                    print("Key is same as self.nodeName, CONTINUING...")
+                    continue                 # This is out own key, do nothing
+                if key in self.routingDB:    # If key is present in routingDB
+                    
+                    if currentValue[1] > newValue[1]:     # If current hop count is greater than received hop count, replace with new
+                        self.routingDB[key] = newValue 
+                    print("routingDB after entry::", self.routingDB)
+                else:                        # key is not present in DB, so make a new entry
+                    self.routingDB[key] = newValue
             
     def broadcastDatabase(self):
         while(True):
@@ -111,7 +121,7 @@ class Node:
             currTime = time.time()
             for addr in list(self.nodeTimestampList):
                 print("checkNodes:: Checking if node ", addr, " is present...")
-                if currTime - self.nodeTimestampList[addr] > 30:
+                if currTime - self.nodeTimestampList[addr] > 180:
                     #Remove the address and timestamp from the list
                     print("Removing node ", addr, " from list, as difference between timestamps is: ", currTime - self.nodeTimestampList[addr])
                     self.nodeTimestampList.pop(addr)
@@ -119,7 +129,7 @@ class Node:
                     self.routingDB.pop(addr)
                     print("removed")
                     
-            time.sleep(5)
+            time.sleep(60)
         
     def start(self, nodeName):
         self.setNodeName()
@@ -159,14 +169,11 @@ class Node:
             self.sock.sendto(self.nodeName.encode('utf-8'), (self.mcast_grp, MCAST_DISC_PORT))
             print(self.nodeName, " has broadcasted...")
             time.sleep(30)
-            
-    
-    def sendMsgToAll(strMsg):
-        sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, MULTICAST_TTL)
-        sock.sendto(b"strMsg", (self.mcast_grp, MCAST_DISC_PORT))
-        
-    def sendMsgToNode(strMsg, ip_addr):
+
+    def unicastNode(strMsg, nodeName):
         unicastSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+        record = self.routingDB[nodeName]
+        ip_addr = record[0]
         unicastSock.sendto(strMsg, ip_addr)
 
 def main():
