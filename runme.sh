d18e9d7833eb6f5e9b69ce4a408e90e7af01a91f
PathakPratik <rajpratik10@gmail.com>
2021-12-10 13:11:47 +0000

    added bash script for emulation, fixed sos issue

---
+++ runme.sh
@@ -0,0 +1,35 @@
+#for nodename in root node1 node2 node3 node4;
+#do
+#       python3 node.py $nodename network1 &
+#       if ["$nodename" == "root"]; then
+#               rootid = $!
+#       fi
+#done
+
+#sleep 30s
+#echo "-----!!!!$rootid!!!------"
+#kill $rootid
+
+
+
+python3 node.py root $1 &
+rootid=$!
+python3 node.py node1 $1 &
+node1id=$!
+python3 node.py node2 $1 &
+node2id=$!
+python3 node.py node3 $1 &
+node3id=$!
+python3 node.py node4 $1 &
+node4id=$!
+
+sleep 30s
+
+kill $rootid
+
+sleep 3m
+
+kill $node1id
+kill $node2id
+kill $node3id
+kill $node4id
